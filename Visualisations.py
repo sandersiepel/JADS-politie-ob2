e661e906100afe310b1aff152e949a215821d62b
sandersiepel <Qnwzcs2qw@@>
2023-08-14 15:17:45 +0200

    added line graph plot that visualizes mean acc per number of horizon days

--- Visualisations.py
+++ Visualisations.py
@@ -338,32 +338,54 @@ class ModelPerformanceVisualizer():
                 self.scores = pickle.load(f)
 
         self.name = pkl_file_name
-        self.prepare_data()
-        self.make_heatmap()
-    
-    def prepare_data(self):
-        # Prepare data for the heatmap
-        heatmap_data = []
 
-        for training_size, forecast_scores in self.scores.items():
-            training_days = int(training_size.split("_")[-1])
-            avg_values = [np.mean(score_list) for score_list in forecast_scores.values()]
-            heatmap_data.append([training_days, *avg_values])
+        self.heatmap()
+        self.accuracy_horizons()
 
-        # Create a DataFrame from the data
-        df = pd.DataFrame(heatmap_data, columns=["Training Days", *forecast_scores.keys()])
-        self.df = df.set_index('Training Days')
-        self.df.index.names = ['Number of days used for training']
-        self.df.columns = self.df.columns.str.split('_').str[-1]
+    def accuracy_horizons(self):
+        new_scores = defaultdict(dict)
+        for k, _ in self.scores.items():
+            # key (k) is "number of training days"
+            for x, y in self.scores[k].items():
+                h = x.split("_")[-1]
+                new_scores[h].extend(y)
+
+        means = [np.mean(v) for _, v in new_scores.items()]
+
+        # Plot results in a line graph
+        fig = plt.figure(figsize=(6, 4))
+        fig.savefig('output/model_performance_per_horizon.png', dpi=600)
+        plt.plot(means)
+        plt.clf()
 
 
-    def make_heatmap(self):
-        self.df = self.df.iloc[::-1,::-1].T # Reverse both rows and columns, and then transpose (to swap axes)
-        fig, ax = plt.subplots(figsize=(10,5))
-        sns.heatmap(self.df, cmap="Blues", xticklabels = 10, yticklabels=7)
-        plt.ylabel('Number of days into the future')
-        plt.xticks(rotation=0) 
-        plt.savefig(f"output/{self.name}.png")
-        # plt.show()
-        # print(f"Saved model performance heatmap to output/{self.name}.png")
-        plt.clf() # Clear figure command to avoid stacking axes in consecutive plots.
+    def heatmap(self) -> None:
+        def prepare_data(self) -> None:
+            # Prepare data for the heatmap
+            heatmap_data = []
+
+            for training_size, forecast_scores in self.scores.items():
+                training_days = int(training_size.split("_")[-1])
+                avg_values = [np.mean(score_list) for score_list in forecast_scores.values()]
+                heatmap_data.append([training_days, *avg_values])
+
+            # Create a DataFrame from the data
+            df = pd.DataFrame(heatmap_data, columns=["Training Days", *forecast_scores.keys()])
+            self.df = df.set_index('Training Days')
+            self.df.index.names = ['Number of days used for training']
+            self.df.columns = self.df.columns.str.split('_').str[-1]
+
+        def make_heatmap(self) -> None:
+            self.df = self.df.iloc[::-1,::-1].T # Reverse both rows and columns, and then transpose (to swap axes)
+            fig, ax = plt.subplots(figsize=(10,5))
+            sns.heatmap(self.df, cmap="Blues", xticklabels = 10, yticklabels=7)
+            plt.ylabel('Number of days into the future')
+            plt.xticks(rotation=0) 
+            plt.savefig(f"output/{self.name}.png")
+            # plt.show()
+            # print(f"Saved model performance heatmap to output/{self.name}.png")
+            plt.clf() # Clear figure command to avoid stacking axes in consecutive plots.
+
+        prepare_data()
+        make_heatmap()
+
