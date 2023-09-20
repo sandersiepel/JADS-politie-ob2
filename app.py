f975c01c524cd7791c99ccc8d6fc5e0266069b9f
sandersiepel <Qnwzcs2qw@@>
2023-09-20 20:40:53 +0200

    test

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Initialize parameters.
-data_source = "google_maps"  # Can be either 'google_maps' or 'routined'.
+data_source = "routined"  # Can be either 'google_maps' or 'routined'.
 # hours_offset is used to offset the timestamps to account for timezone differences. For google maps, timestamp comes in GMT+0
 # which means that we need to offset it by 2 hours to make it GMT+2 (Dutch timezone). Value must be INT!
-hours_offset = 2 # Should be 0 for routined and 2 for google_maps. 
+hours_offset = 0 # Should be 0 for routined and 2 for google_maps. 
 # begin_date and end_date are used to filter the data for your analysis.
-begin_date = "2023-01-01"
-end_date = "2023-09-11"  # End date is INclusive! 
+begin_date = "2023-05-05"
+end_date = "2023-09-19"  # End date is INclusive! 
 # FRACTION is used to make the DataFrame smaller. Final df = df * fraction. This solves memory issues, but a value of 1 is preferred.
-fraction = 0.5
+fraction = 1
 # For the model performance class we need to specify the number of training days (range) and testing horizon (also in days)
 training_window_size = 100
 horizon_size = 30
